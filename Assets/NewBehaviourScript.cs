e292fa8f8c7f1d976b39806bfc1f125327c52b65
michelle lavryssen <1010199@student.sae.edu.au>
2018-08-22 16:58:15 +1000

    mesh now appears as 3D cube. need to do Sin and Cos

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -28,8 +28,11 @@ namespace Michelle
         public int numberOfStars;
         public Mesh MyMesh;
         public float speed;
+        public Vector3 MeshTranslate;
+        public Vector3 MeshScale;
+        public float MeshRotate;
 
-    
+
 
         // Use this for initialization
         void Start()
@@ -43,27 +46,28 @@ namespace Michelle
 
 
 
-            MyMesh = new Mesh(new Vector3(Random.Range(-1000, 1000), Random.Range(-1000, 1000)));
+            MyMesh = new Mesh(new Vector3(0, 0, 0));
 
             for (int i = 0; i < numberOfStars; i++)
             {
-                StarClass starClass = new StarClass(new Vector3(Random.Range(-1000, 1000), Random.Range(-1000, 1000),
-                    Random.Range(10, 100)));
-                
+                StarClass starClass = new StarClass(new Vector3(Random.Range(-128, 128), Random.Range(-128, 128),
+                    Random.Range(5, 10)));
+
                 MyMesh.Vertexes.Add(starClass);
 
-                
+
 
             }
-            MyMesh.Position = new Vector3(Random.Range(-1000, 1000), Random.Range(-1000, 1000));
-            MyMesh.Vertexes[0].LocalPosition = new Vector3(-0.5f, 0.5f, 0.5f);
-            MyMesh.Vertexes[1].LocalPosition = new Vector3(-0.5f, 0.5f, -0.5f);
-            MyMesh.Vertexes[2].LocalPosition = new Vector3(0.5f, 0.5f, 0.5f);
-            MyMesh.Vertexes[3].LocalPosition = new Vector3(0.5f, 0.5f, -0.5f);
-            MyMesh.Vertexes[4].LocalPosition = new Vector3(-0.5f, -0.5f, 0.5f);
-            MyMesh.Vertexes[5].LocalPosition = new Vector3(-0.5f, -0.5f, -0.5f);
-            MyMesh.Vertexes[6].LocalPosition = new Vector3(0.5f, -0.5f, 0.5f);
-            MyMesh.Vertexes[7].LocalPosition = new Vector3(0.5f, -0.5f, -0.5f);
+
+            MyMesh.Vertexes[0].LocalPosition = new Vector3(-20, 20, 20);
+            MyMesh.Vertexes[1].LocalPosition = new Vector3(20, 20, 20);
+            MyMesh.Vertexes[2].LocalPosition = new Vector3(-20, 20, 10);
+            MyMesh.Vertexes[3].LocalPosition = new Vector3(20, 20, 10);
+            MyMesh.Vertexes[4].LocalPosition = new Vector3(-20, -20, 20);
+            MyMesh.Vertexes[5].LocalPosition = new Vector3(20, -20, 20);
+            MyMesh.Vertexes[6].LocalPosition = new Vector3(-20, -20, 10);
+            MyMesh.Vertexes[7].LocalPosition = new Vector3(20, -20, 10);
+
 
 
 
@@ -95,78 +99,111 @@ namespace Michelle
         }
 
         void Update()
-        { 
-    
+        {
+
             ClearBuffer();
 
 
-            MyMesh.Position.z -= speed;
+            //MyMesh.Position.z -= speed;
 
-                if (MyMesh.Position.z < 0)
-                {
-                    MyMesh.Position.z = MyMesh.zStartingPoint;
-                }
+            //    if (MyMesh.Position.z < 0)
+            //    {
+            //        MyMesh.Position.z = MyMesh.zStartingPoint;
+            //    }
 
 
 
 
             foreach (StarClass item in MyMesh.Vertexes)
             {
-                Vector3 WorldPosition = item.LocalPosition + MyMesh.Position;
+                Vector3 WorldPosition;
+                WorldPosition.x = (item.LocalPosition.x + MyMesh.Position.x) * MeshScale.x;
+                WorldPosition.y = (item.LocalPosition.y + MyMesh.Position.y) * MeshScale.y;
+                WorldPosition.z = (item.LocalPosition.z + MyMesh.Position.z) * MeshScale.z;
 
-                setPixel((int)(item.LocalPosition.x / (item.LocalPosition.z * FoV) + offsetX),
-                   (int)(item.LocalPosition.y / (item.LocalPosition.z * FoV) + offsetY),
+
+
+                setPixel((int)(WorldPosition.x / (WorldPosition.z * FoV) + offsetX),
+                   (int)(WorldPosition.y / (WorldPosition.z * FoV) + offsetY),
                     (byte)(colourR - ((item.LocalPosition.z / 100) * colourR)),
                     (byte)(colourG - ((item.LocalPosition.z / 100) * colourG)),
                     (byte)(colourB - ((item.LocalPosition.z / 100)) * colourB));
             }
+            MeshRotate -= Time.deltaTime;
+            RotateMesh(MeshRotate);
             tex.LoadRawTextureData(backbuffer);
             tex.Apply(false);
 
         }
 
+
+        public void TranslateMesh()
+        {
+
+            MyMesh.Position = MeshTranslate;
+            print("this should be moving the mesh");
+
+
+
+        }
+
+        public void RotateMesh(float degrees)
+        {
+            foreach (StarClass item in MyMesh.Vertexes)
+            {
+                Vector3 localPosClone;
+
+                localPosClone.x = item.LocalPosition.x;
+                localPosClone.y = item.LocalPosition.y;
+                localPosClone.z = item.LocalPosition.z;
+
+                localPosClone.x *= Mathf.Sin(degrees);
+
+                item.LocalPosition = localPosClone;
+            }
+        }
 
 
 
         // Update is called once per frame
         //void UpdateStarDemo()
         //{
-           // ClearBuffer();
-//            foreach (StarClass item in Stars)
-//            {
-//
-//
-//
-//                item.Update();
-//
-//                setPixel((int)(item.LocalPosition.x / (item.LocalPosition.z * FoV) + offsetX),
-//                    (int)(item.LocalPosition.y / (item.LocalPosition.z * FoV) + offsetY),
-//                    (byte)(colourR - ((item.LocalPosition.z / 100) * colourR)),
-//                    (byte)(colourG - ((item.LocalPosition.z / 100) * colourG)),
-//                    (byte)(colourB - ((item.LocalPosition.z / 100)) * colourB));
-//
-//            }
-//
+        // ClearBuffer();
+        //            foreach (StarClass item in Stars)
+        //            {
+        //
+        //
+        //
+        //                item.Update();
+        //
+        //                setPixel((int)(item.LocalPosition.x / (item.LocalPosition.z * FoV) + offsetX),
+        //                    (int)(item.LocalPosition.y / (item.LocalPosition.z * FoV) + offsetY),
+        //                    (byte)(colourR - ((item.LocalPosition.z / 100) * colourR)),
+        //                    (byte)(colourG - ((item.LocalPosition.z / 100) * colourG)),
+        //                    (byte)(colourB - ((item.LocalPosition.z / 100)) * colourB));
+        //
+        //            }
+        //
 
 
-            //for (int i = 0; i < backbuffer.Length; i++)
-            //{
-            //    setPixel(Random.Range(0, xsize), Random.Range(0, ysize), (byte)Random.Range(0,255), (byte)Random.Range(0,255), (byte)Random.Range(0,255));
-            // }
+        //for (int i = 0; i < backbuffer.Length; i++)
+        //{
+        //    setPixel(Random.Range(0, xsize), Random.Range(0, ysize), (byte)Random.Range(0,255), (byte)Random.Range(0,255), (byte)Random.Range(0,255));
+        // }
 
 
-            //      setPixel(3, 2, 255, 255, 255)
-            //    setPixel(6, 0, 0, 0, 255);
+        //      setPixel(3, 2, 255, 255, 255)
+        //    setPixel(6, 0, 0, 0, 255);
 
 
-            //  backbuffer[0] = 255;
-            //  backbuffer[1] = 0;
-            // backbuffer[2] = 0;
+        //  backbuffer[0] = 255;
+        //  backbuffer[1] = 0;
+        // backbuffer[2] = 0;
 
-            //upload texture
-            tex.LoadRawTextureData(backbuffer);
-            tex.Apply(false);
-        }
+        //upload texture
+        //tex.LoadRawTextureData(backbuffer);
+        // tex.Apply(false);
+        //}
 
     }
 }
